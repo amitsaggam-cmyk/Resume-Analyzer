27a7ad7deaadddb64312a7c78619a47677f2de51
Amit Saggam <amit.saggam@jadeglobal.com>
2026-04-07 02:26:59 +0530

    Updated API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://resume-analyzer-qh4k.onrender.com';
 
 export const uploadResume = async (file) => {
   const formData = new FormData();
